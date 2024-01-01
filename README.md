12b6a8f4a1122e6e09aa5e28f5325d470a9c11aa
yuri.dunets <yuri.dunets@hiqo-solutions.com>
2024-01-01 12:50:15 +0400

    init: github action education repo

---
+++ README.md
@@ -0,0 +1,2 @@
+Github Actions Education
+========================
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>GitHub Actions Education</h1>
+    </>
+  )
+}
+
+export default App
