78dd98334d9dcd693c3773756b2e3438a9ae4420
yuri.dunets <yuri.dunets@hiqo-solutions.com>
2024-02-19 22:06:30 +0400

    feat: added test workflow

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import {render, screen} from "@testing-library/react";
+import App from "./App.tsx";
+
+describe('App component', () => {
+  it('should render app component', async () => {
+    render(<App/>)
+
+    expect(screen.getByText('GitHub Actions Education')).toBeInTheDocument()
+  })
+})
